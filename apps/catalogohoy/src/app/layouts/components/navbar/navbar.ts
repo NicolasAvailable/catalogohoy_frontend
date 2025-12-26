149c359ac72dae98b0cae664a03c4cef92ceeffc
Nicolás Soto <105766645+NicolasAvailable@users.noreply.github.com>
2025-12-26 10:12:57 -0400

    refactor(navbar, profile-menu): remove unused RouterLinkActive import

--- apps/catalogohoy/src/app/layouts/components/navbar/navbar.ts
+++ apps/catalogohoy/src/app/layouts/components/navbar/navbar.ts
@@ -1,12 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterLinkActive } from '@angular/router';
 import { ProfileStore } from '@catalogohoy/profile';
 import { AvatarComponent, ButtonComponent } from '@ui';
 import { ProfileMenu } from './components';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLinkActive, AvatarComponent, ButtonComponent, ProfileMenu],
+  imports: [AvatarComponent, ButtonComponent, ProfileMenu],
   templateUrl: './navbar.html',
 })
 export class Navbar {
